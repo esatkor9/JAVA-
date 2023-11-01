aa4f20fe1cce2ff123d1ec44ed91872e9d6e1fe8
Esat Kor <80432391+esatkor9@users.noreply.github.com>
2023-11-01 17:01:24 +0300

    kullanicıiirisiKontrolu

---
+++ kullaniciKontrol.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class kullaniciKontrol {
+    public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    String userName , password ;
+    int degisim;
+        System.out.print("Lütfen kullanıcı adını giriniz = ");
+            userName = input.nextLine();
+        System.out.print("Lütfen şifrenizi giriniz =");
+            password = input.nextLine();
+
+         if (userName.equals("Esat.Kor")){
+             if (password.equals("Esatgs123#")) {
+                 System.out.print("Sisteme hoşgeldiniz");
+             }else
+                 System.out.print("Şifrenizi hatalı değiştirmek istermisiniz Evet ise 1 ,Hayır ise 2 basın");
+                 degisim = input.nextInt();
+
+                 switch (degisim){
+
+                     case 1 :
+                        System.out.print("Lütfen yeni şifrenizi giriniz =");
+                        input.nextLine();
+                        password = input.nextLine();
+                            if (password.equals("Esatgs123#")){
+                            System.out.print("Şifre oluşturulamadı, lütfen başka şifre giriniz");
+                            }else
+                                System.out.println("Şifre oluşturuldu");
+                        break;
+                     case 2 :
+                         System.out.print("İyi günler dileriz.");
+                        break;
+                 }
+
+
+         }else
+             System.out.print("Lütfen kullanıcı adınızı kontrol ediniz.");
+
+
+}
+}
